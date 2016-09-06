1758fd4795081ad2b9026f1dd49e6e319be74a63
vanjadjuric <vanjadjuric25@gmail.com>
2016-09-06 23:17:49 +0200

    Added application files
    Still only basic work done, no db comunication.

---
+++ ToDo-Calendar/ToDo-Calendar/Controllers/FullWeekController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ToDo_Calendar.Models;
+
+namespace ToDo_Calendar.Controllers
+{
+    public class FullWeekController : Controller
+    {
+        // GET: FullWeek
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SelectedDay(int id)
+        {
+            DayModel day = new DayModel();
+            day.id = id;
+            return View(day);
+        }
+    }
+}
---
+++ ToDo-Calendar/ToDo-Calendar/Models/DayModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDo_Calendar.Models
+{
+    public class DayModel
+    {
+        public int id { get; set; }
+    }
+}
